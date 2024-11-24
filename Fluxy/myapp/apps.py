aaa9bf112afad91bec8f05593ca1519c7fd20b4f
1unark <157058852+1unark@users.noreply.github.com>
2024-11-23 22:15:24 -0800

    tornado spinnnn

--- Fluxy/myapp/apps.py
+++ Fluxy/myapp/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+def app(environ, start_response):
+    pprint("whoo hooo.  ")
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
