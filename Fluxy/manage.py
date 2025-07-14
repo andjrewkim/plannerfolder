d165e210e31ccee3b3974a33356e7cbb6833c6db
1unark <157058852+1unark@users.noreply.github.com>
2025-07-14 14:00:08 -0700

    manage.py create superuser

--- Fluxy/manage.py
+++ Fluxy/manage.py
@@ -25,3 +25,18 @@ if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
     main()
+
+
+
+
+# Add this near the bottom of manage.py or wsgi.py — right before the main execution starts
+
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fluxy.settings")
+django.setup()
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "vexr0264@gmail.com", "Horosny1414!")
