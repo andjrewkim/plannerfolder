17bd9ebc695dfc5365339462d039fc439b1c3e8d
1unark <157058852+1unark@users.noreply.github.com>
2025-08-14 22:50:11 -0700

    debug rpint

--- Fluxy/manage.py
+++ Fluxy/manage.py
@@ -6,6 +6,7 @@ import sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fluxy.settings')
 
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 
 def main():
     """Run administrative tasks."""
