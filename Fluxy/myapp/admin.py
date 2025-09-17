c3dfdad81a7451857edef19e53a75a2e4c964abe
1unark <157058852+1unark@users.noreply.github.com>
2025-09-16 18:47:40 -0700

    admin

--- Fluxy/myapp/admin.py
+++ Fluxy/myapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, PlannerClass, Assignment, CalendarEvent, TodoTask, UserSettings, NoteTab
+from .models import CustomUser, PlannerClass, Assignment, CalendarEvent, TodoTask, UserSettings, NoteTab, NoWorkDay
 
 
 admin.site.register(CalendarEvent)
@@ -13,3 +13,4 @@ admin.site.register(UserSettings)
 admin.site.register(PlannerClass)
 admin.site.register(Assignment)
 admin.site.register(NoteTab)
+admin.site.register(NoWorkDay)
