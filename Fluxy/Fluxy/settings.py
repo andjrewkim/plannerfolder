ebadbb2e31c089ef8ff2dd9d6bc8516d74c1df2a
1unark <157058852+1unark@users.noreply.github.com>
2025-11-27 12:28:27 -0800

    sending emails.py

--- Fluxy/Fluxy/settings.py
+++ Fluxy/Fluxy/settings.py
@@ -5,7 +5,18 @@ import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # change if not Gmail
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 load_dotenv()  # Load from .env file
+
 
 PORT = os.getenv('PORT', 8080)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
